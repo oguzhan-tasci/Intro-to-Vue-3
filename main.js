63bf850a460f7bae6c29c9800787332f0219bd96
oyeProjects <oyeprojects@hotmail.com>
2022-07-23 20:38:00 +0300

    I finished chapter 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            person : "Elif",
+            description : "I love my nephew and nieces"
+        }
+    },
+})
