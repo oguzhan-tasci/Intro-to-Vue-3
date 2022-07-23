7515472712d854410b62feb7f45f5755784ad520
oyeProjects <oyeprojects@hotmail.com>
2022-07-24 00:23:37 +0300

    computed learned but review again

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant : 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            variants: [{
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                    quantity : 50
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
+                    quantity : 0
+                },
             ]
         }
     },
@@ -17,8 +25,32 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+            // console.log(index); Green : 0 , Blue : 1 
+        }
+    },
+
+    // The benefit of computer properties is that they cash the value. So, it stores it away and only updates when it needs to.
+    // When one of its dependencies change(Boosts performance).
+    // For example, if the brand were to change from 'Vue Mastery' to 'Node Mastery',
+    // our computer property would receive that new dependency then recalculate and return the new value.
+
+                // Update image & inStock using computed properties
+    // Each variant now has a quantity, and our green socks have 50 while the blue socks have zero.
+    //  In other words, the blue socks are out of stock.
+    //  Let's refactor our code, so we're updating the image and in stock with computer properties.
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity; 
         }
     }
+
 })
