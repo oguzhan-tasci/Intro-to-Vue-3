fa0075eb62bbb195fcddf7b6b26032d5cc2d95a4
oyeProjects <oyeprojects@hotmail.com>
2022-07-24 00:50:59 +0300

    I learned emit

--- main.js
+++ main.js
@@ -5,5 +5,14 @@ const app = Vue.createApp({
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(){
+            this.cart += 1;
+        }
+    }
 })
+
+// We already know that 'props' are a way to pass data down into a component,
+// but what about when something happens within that component, like a 'button click' ? 
+// How do we let other parts of our app know that event happened ?
+// The answer is to 'emit' that event, telling the parent that it happened.
